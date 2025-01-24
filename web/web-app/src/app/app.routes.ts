0a40d9c9119fc028a17c5a81446fb51279ff4bf8
Lina Mandresy <mandresyrab@gmail.com>
2025-01-24 15:19:43 -0500

    Set Up display

--- web/web-app/src/app/app.routes.ts
+++ web/web-app/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/auth',
+        pathMatch: 'full',
+    },{
+        path: 'auth',
+        component: AuthComponent,
+        children: [
+            {
+                path:'',
+                redirectTo: 'login',
+                pathMatch:'prefix'
+            },{
+                path: 'login',
+                component: LoginComponent
+            }
+
+        ]
+    }];
---
+++ web/web-app/src/app/env/config.ts
@@ -0,0 +1,3 @@
+export enum Config {
+    baseUrl = 'http://localhost:3000',
+}
